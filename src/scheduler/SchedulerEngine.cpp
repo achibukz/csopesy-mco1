aca07d63237053c731b67fcc77949d95475786b4
aki bukz <akibukzwork@gmail.com>
2026-06-25 00:53:32 +0900

    upd: change tick to 1ms

--- src/scheduler/SchedulerEngine.cpp
+++ src/scheduler/SchedulerEngine.cpp
@@ -66,7 +66,7 @@ void SchedulerEngine::stepOnce() {
 
 void SchedulerEngine::tickLoop() {
     while (running_.load()) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
         if (!running_.load()) break;
         tick_.fetch_add(1);
         tickSleepingProcesses();
